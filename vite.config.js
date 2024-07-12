4ec1c9e6397e3a55ef38535fb28acb205e25f31d
sanchezZin <sanchezhanszin0@gmail.com>
2024-07-12 14:01:57 +0800

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-job-management-system/",
   plugins: [react()],
   server: {
     port: 5173,
